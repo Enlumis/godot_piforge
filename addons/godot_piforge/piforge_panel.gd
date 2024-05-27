322bbcf37294a19789a233baab814a7f7032aaf8
Antoine Lamy <antoinelamy0375@gmail.com>
2024-05-27 06:08:42 -0700

    Add support for mask

--- addons/godot_piforge/piforge_panel.gd
+++ addons/godot_piforge/piforge_panel.gd
@@ -17,7 +17,9 @@ var is_auth_ready:bool = false
 var credits = 0
 var _headers : PackedStringArray = []
 var export_type = "jpg"
-var current_ai_product = "core_ai"
+var draw_mode = 0
+var mask: Image = null
+var drawing: bool = false
 
 var history: Array[Dictionary] = [
 	{
@@ -69,23 +71,28 @@ var history: Array[Dictionary] = [
 var product_list: Array[Dictionary] = [
 	{
 		"name": "Core AI (Recommended)",
-		"id": "core_ia",
+		"id": "core_ai",
+		"has_mask": true,
 	},
 	{
 		"name": "Ultra",
 		"id": "ultra",
+		"has_mask": true,
 	},
 	{
 		"name": "Flat Style",
 		"id": "flat_style_core_ai",
+		"has_mask": true,
 	},
 	{
 		"name": "Vehicle",
 		"id": "dgv_core_ai",
+		"has_mask": true,
 	},
 	{
 		"name": "Open Journey",
 		"id": "openjourney",
+		"has_mask": true,
 	},
 	{
 		"name": "SDXL",
@@ -94,6 +101,7 @@ var product_list: Array[Dictionary] = [
 	{
 		"name": "SD 1.5",
 		"id": "sd15",
+		"has_mask": true,
 	},
 	# upscaler not available yet
 	#{ 
@@ -101,6 +109,7 @@ var product_list: Array[Dictionary] = [
 	#	"id": "upscaler",
 	#},
 ]
+var current_ai_product: Dictionary
 
 @onready var auth_panel:VBoxContainer = $AuthenticatePanel
 @onready var auth_panel_spinner:Control = $AuthenticatePanel/Spinner
@@ -125,7 +134,19 @@ var product_list: Array[Dictionary] = [
 
 @onready var actions_buttons:Control = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/ActionButtons
 @onready var canvas_spinner:Control = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/Spinner
-@onready var canvas_subject_item:TextureRect = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/TextureRect
+@onready var canvas_subject_item:TextureRect = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/MarginContainer/TextureRect
+@onready var canvas_subject_mask:TextureRect = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/MarginContainer/TextureRectMask
+@onready var graph_node:GraphNode = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode
+
+@onready var draw_mask_bar:Control = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/DrawMaskBarFull
+@onready var draw_mask_button:Button = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/ButtonDrawMask
+@onready var draw_mask_button_delete:Button = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/DrawMaskBarFull/ButtonDrawMaskDelete
+@onready var draw_mask_button_pen:CheckBox = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/DrawMaskBarFull/DrawMaskBar2/CheckBoxPencil
+@onready var draw_mask_button_eraser:CheckBox = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/DrawMaskBarFull/DrawMaskBar2/CheckBoxEraser
+@onready var draw_mask_info:Label = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/DrawMaskBarFull/LabelDrawMask
+@onready var draw_mask_pencil_size:HSlider = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/DrawMaskBarFull/DrawMaskBar2/HSlider
+@onready var draw_mask_invert:Button = $TabContainer/Generate/HSplitContainer/HSplitContainer/ScrollContainer/GraphNode/VBoxContainer/DrawMaskBarFull/DrawMaskBar2/ButtonInvert
+@onready var label_has_mask:Label = $TabContainer/Generate/HSplitContainer/HSplitContainer/BoxContainer2/ScrollContainer/VBoxContainer/HasMask
 
 @onready var settings_credits:Label = $TabContainer/Settings/Credits
 @onready var generator_credits:Label = $TabContainer/Generate/HSplitContainer/HSplitContainer/BoxContainer2/ScrollContainer/VBoxContainer/Credits
@@ -163,6 +184,7 @@ func _enter_tree():
 
 
 func _ready():
+	current_ai_product = product_list[0]
 	
 	editor_file_dialog = EditorFileDialog.new()
 	add_child(editor_file_dialog)
@@ -178,12 +200,26 @@ func _ready():
 		var prod = product_list[i]
 		product_select.add_item(prod["name"], i)
 	product_select.get_popup().id_pressed.connect(_on_change_ai_product)
+	
+	draw_mask_button.connect("pressed", _on_pressed_draw_mask_button)
+	draw_mask_button_delete.connect("pressed", _on_pressed_draw_mask_button_delete)
+	draw_mask_button_pen.connect("pressed", _on_pressed_draw_mask_button_pen)
+	draw_mask_button_eraser.connect("pressed", _on_pressed_draw_mask_button_eraser)
+	draw_mask_invert.connect("pressed", _on_pressed_draw_mask_invert)
+	show_draw_mask_tools(false)
+	draw_mask_button.visible = false
+	
 
 
 func _on_change_ai_product(id: int):
+	show_draw_mask_tools(false)
 	var prod = product_list[id]
-	current_ai_product = prod["id"]
-	pass
+	current_ai_product = prod
+	if canvas_subject_item.visible and canvas_subject_item.texture != null:
+		if !current_ai_product.has("has_mask"):
+			draw_mask_button.visible = false
+		else:
+			draw_mask_button.visible = true
 	
 	
 func _process(delta):
@@ -212,6 +248,8 @@ func on_open_panel():
 		tab_container.visible = true
 	if empty_image:
 		actions_buttons.visible = false
+		draw_mask_bar.visible = false
+		draw_mask_info.visible = false
 	save_button.icon = EditorInterface.get_editor_theme().get_icon("Save", 'EditorIcons')
 	_on_button_check_key_pressed()
 
@@ -372,9 +410,12 @@ func _on_button_generate_pressed():
 	canvas_spinner.visible = true
 	canvas_subject_item.visible = false
 	actions_buttons.visible = false
+	draw_mask_bar.visible = false
+	draw_mask_info.visible = false
 	generate_status.text = ""
 	generate_spinner.create_tween().tween_property(generate_spinner.get_child(0), "rotation_degrees", 1000000, 1000).from(0)
 	canvas_spinner.create_tween().tween_property(canvas_spinner.get_child(0), "rotation_degrees", 1000000, 1000).from(0)
+	
 	
 	var img_count = int(image_count_spinbox.value)
 	
@@ -384,28 +425,44 @@ func _on_button_generate_pressed():
 	
 	var params = {
 		"prompt": prompt_text.text,
-		"product": current_ai_product,
+		"product": current_ai_product["id"],
 		"denoise": denoise_slider.value,
 		"cfg": cfg_slider.value,
 		"image_count": img_count,
 	}
+	if current_ai_product.has("has_mask"):
+		if canvas_subject_mask.visible:
+			var mask_data = get_base_64_mask_data(get_real_mask())
+			params["mask"] = "data:image/png;base64,%s" % mask_data
 	if input_image_data != "":
 		params["image_url"] = "data:image/png;base64,%s" % input_image_data
+		
+	show_draw_mask_tools(false)
+	draw_mask_button.visible = false
+	
 	get_images(JSON.stringify(params), func(success, data):
 		generate_spinner.visible = false
 		generate_button.disabled = false
 		if success:
-			if data.has("total_credit_cost"):
-				credits -= data["total_credit_cost"]
-				set_credits(credits)
-			for i in range(img_count):
-				var h = historyItems[i]
-				var create_history = get_json_data(JSON.stringify(data))
-				create_history["data"] = get_json_data(JSON.stringify(data))
-				create_history["data"]["img_url"] = data["img_urls"][i]
-				h.setData(create_history)
-				add_item_history_action(h)
-			download_image(data["img_urls"][0])
+			if data.has("data") and data["data"].has("queue_status") and data["data"]["queue_status"] == "failed":
+				generate_status.text = JSON.stringify(data)
+				canvas_spinner.visible = false
+				
+				for i in range(img_count):
+					var h = historyItems[i]
+					h.queue_free()
+			else:
+				if data.has("total_credit_cost"):
+					credits -= data["total_credit_cost"]
+					set_credits(credits)
+				for i in range(img_count):
+					var h = historyItems[i]
+					var create_history = get_json_data(JSON.stringify(data))
+					create_history["data"] = get_json_data(JSON.stringify(data))
+					create_history["data"]["img_url"] = data["img_urls"][i]
+					h.setData(create_history)
+					add_item_history_action(h)
+				download_image(data["img_urls"][0])
 		else:
 			generate_status.text = JSON.stringify(data)
 			canvas_spinner.visible = false
@@ -416,8 +473,11 @@ func _on_button_generate_pressed():
 		pass)
 
 
+
 func set_result(item):
 	actions_buttons.visible = false
+	draw_mask_bar.visible = false
+	draw_mask_info.visible = false
 	if item.has("data"):
 		var outdata = item["data"]
 		if outdata.has("img_url"):
@@ -429,6 +489,9 @@ func download_image(url:String):
 	canvas_spinner.visible = true
 	canvas_spinner.create_tween().tween_property(canvas_spinner.get_child(0),"rotation_degrees", 1000000, 1000).from(0)	
 	canvas_subject_item.visible = false
+	show_draw_mask_tools(false)
+	draw_mask_button.visible = false
+	
 
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
@@ -452,6 +515,9 @@ func _http_request_completed(result, response_code, headers, body):
 	var texture = ImageTexture.create_from_image(image)
 	canvas_subject_item.texture = texture
 	actions_buttons.visible = true
+	draw_mask_bar.visible = true
+	draw_mask_info.visible = true
+	show_draw_mask_tools(false)
 	canvas_subject_item.visible = true
 	canvas_spinner.visible = false
 	empty_image = false
@@ -473,7 +539,6 @@ func check_api_key(callback:Callable):
 func get_images(params, callback:Callable):
 	var authReq = "Authorization: Bearer %s" % get_api_key()
 	_headers=[authReq, "Content-Type: application/json; charset=UTF-8"]
-	
 	http_heq("images", func(result : int, response_code : int, headers : PackedStringArray, body : PackedByteArray, request : HTTPRequest) -> void:
 		var bodutf = body.get_string_from_utf8()
 		var json = get_json_data(bodutf)
@@ -590,3 +655,168 @@ func _on_button_input_copy_img_pressed():
 func _on_button_start_generate_pressed():
 	if is_auth_ready:
 		tab_container.current_tab = 0
+
+
+func clear_img_mask():
+	mask = null
+	canvas_subject_mask.texture = null
+
+
+func invert_mask():
+	if mask == null:
+		create_new_mask(canvas_subject_item.texture.get_width(), canvas_subject_item.texture.get_height())
+	for nx in range(0,mask.get_width()):
+		for ny in range(0,mask.get_height()):
+			var col = mask.get_pixel(nx, ny)
+			col.a = 1 - col.a * 2
+			if col.a > 0.5:
+				col.a = 0.5
+			if col.a < 0:
+				col.a = 0
+			mask.set_pixel(nx, ny, col)
+	var texture = ImageTexture.create_from_image(mask)
+	canvas_subject_mask.texture = texture
+
+
+var mask_w = 0
+var mask_h = 0
+
+func create_new_mask(w,h):
+	mask_w = w
+	mask_h = h
+	mask = Image.create(w,h, false,Image.FORMAT_RGBA8)
+	mask.fill(Color(0.8,0,0,0))
+
+
+func get_real_mask():
+	var real_mask = Image.create(mask.get_width(), mask.get_height(), false, Image.FORMAT_RGBA8)
+	real_mask.fill(Color(0,0,0,0))
+	for nx in range(0,real_mask.get_width()):
+		for ny in range(0,real_mask.get_height()):
+			var col = mask.get_pixel(nx, ny)
+			if col.a > 0.25:
+				real_mask.set_pixel(nx, ny, Color.WHITE)
+			else:
+				real_mask.set_pixel(nx, ny, Color(0,0,0,0))
+	return real_mask
+
+
+func _update_image(p:Vector2):
+	_on_texture_rect_mask_mouse_entered()
+	if mask == null:
+		create_new_mask(canvas_subject_item.texture.get_width(), canvas_subject_item.texture.get_height())
+	var radius = draw_mask_pencil_size.value
+	
+	var r_draw = 0.5
+	if draw_mode == 1:
+		r_draw = 0
+	
+	for x in range(0,radius*2):
+		for y in range(0,radius*2):
+			var nx = radius - x
+			var ny = radius - y
+			var distance = sqrt(nx * nx + ny * ny);
+			var power = clamp(radius - distance, 0, 1)
+			nx -= radius/2
+			ny -= radius/2
+			nx += p.x
+			ny += p.y
+			if nx < 0:
+				continue
+			if ny < 0:
+				continue
+			if nx >= mask_w:
+				continue
+			if ny >= mask_h:
+				continue
+			var col = Color(0.8,0.0,0.0,0.0)
+			if draw_mode == 0:
+				col = mask.get_pixel(nx,ny).blend(Color(0.8,0.0,0.0,r_draw * power))
+				col.r = 0.8
+			if col.a > 0.5:
+				col.a = 0.5
+			if col.a < 0:
+				col.a = 0
+			mask.set_pixel(nx, ny, col)
+	
+	var texture = ImageTexture.create_from_image(mask)
+	canvas_subject_mask.texture = texture
+
+
+func get_base_64_mask_data(img:Image):
+	var buf = img.save_png_to_buffer()
+	return Marshalls.raw_to_base64(buf)
+
+
+func _on_pressed_draw_mask_button():
+	show_draw_mask_tools(true)
+
+
+func _on_pressed_draw_mask_button_delete():
+	show_draw_mask_tools(false)
+	clear_img_mask()
+
+
+func set_draw_mode(b:int):
+	draw_mode = b
+	if b == 0:
+		draw_mask_button_eraser.button_pressed = false
+		draw_mask_button_pen.button_pressed = true
+	else:
+		draw_mask_button_eraser.button_pressed = true
+		draw_mask_button_pen.button_pressed = false
+
+
+func _on_pressed_draw_mask_button_pen():
+	set_draw_mode(0)
+
+
+func _on_pressed_draw_mask_button_eraser():
+	set_draw_mode(1)
+
+
+func _on_pressed_draw_mask_invert():
+	invert_mask()
+
+
+func show_draw_mask_tools(b:bool):
+	if !current_ai_product.has("has_mask"):
+		b = false
+	for c in draw_mask_bar.get_children():
+		c.visible = b
+	label_has_mask.visible = b
+	draw_mask_button.visible = !b
+	draw_mask_info.visible = b
+	canvas_subject_mask.visible = b
+	if b:
+		canvas_subject_item.modulate = Color(1,1,1,0.5)
+		set_draw_mode(0)
+		_on_button_input_copy_img_pressed()
+	else:
+		canvas_subject_item.modulate = Color.WHITE
+		if !current_ai_product.has("has_mask"):
+			draw_mask_button.visible = false
+
+
+func _on_texture_rect_mask_gui_input(event):
+	if draw_mask_button.visible:
+		return
+	if event is InputEventMouseButton and event.button_index == 1:
+		drawing = event.pressed
+	if drawing:
+		if event is InputEventMouseMotion or event is InputEventMouseButton:
+			var canvas_size = canvas_subject_mask.get_parent().size
+			var ratio = canvas_size.x / float(mask_w)
+			if canvas_size.x > canvas_size.y:
+				ratio = canvas_size.y / float(mask_h)
+			_update_image(event.position / ratio)
+
+
+func _on_texture_rect_mask_mouse_entered():
+	graph_node.draggable = false
+	graph_node.selectable = false
+
+
+func _on_texture_rect_mask_mouse_exited():
+	graph_node.draggable = true
+	graph_node.selectable = true
